9d2d433190f1268b79076fb6d7018cc53b899e2d
abdinasir97 <abdinasirsheik650@gmail.com>
2026-03-29 15:39:55 +0200

    exercise 7 added

---
+++ Exercise 7/main.js
@@ -0,0 +1,9 @@
+let colors = ["green", "blue", "red"];
+console.log(colors);
+console.log("after push");
+colors.push('yellow');
+console.log(colors);
+
+console.log("after shift");
+colors.shift();
+console.log(colors);
