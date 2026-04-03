5d9ef8a12a46385f50d21ed763b744cdd5e21097
abdinasir97 <abdinasirsheik650@gmail.com>
2026-04-03 14:27:32 +0200

    exercise 10 added

---
+++ js Exercise 10/main.js
@@ -0,0 +1,22 @@
+
+let library = [
+    {
+        title : "the power of decipline",
+        author: "daniel walter",
+        year : 2020,
+    },
+    {
+        title:"high performance habbits",
+        author: "brendon burchard",
+        year: 2022,
+    },
+    {
+        title:"Inspire Greatness",
+        author: "matt tenney",
+        year: 2024,
+    },
+] 
+
+console.log(library)
+console.log(library[0].title)
+console.log(library[1].author)
