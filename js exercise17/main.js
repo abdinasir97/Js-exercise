3431e531a3889f92c1a4e9670983279c4524b2cf
abdinasir <abdinasirsheik650@gmail.com>
2026-05-10 06:53:48 +0200

    exercise 17added

---
+++ js exercise17/main.js
@@ -0,0 +1,10 @@
+let temperature = 24
+if (temperature < 0){
+    console.log("today is very cold")
+}else if (temperature < 15){
+    console.log("waaw its cold")
+}else if (temperature <= 25){
+    console.log("actully its worm")
+}else{
+    console.log("oh my god today is hot")
+}
